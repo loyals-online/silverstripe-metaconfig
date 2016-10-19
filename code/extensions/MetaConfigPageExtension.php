54807dd29ef0c42a678cb2265681f575e84fb01d
Martijn <martijnschenk@loyals.nl>
2016-10-19 13:12:13 +0200

    Added google scripts (gtm/ga)

--- code/extensions/MetaConfigPageExtension.php
+++ code/extensions/MetaConfigPageExtension.php
@@ -48,6 +48,19 @@ class MetaConfigPageExtension extends DataExtension
     }
 
     /**
+     * Hook into the onBeforePageInit extend call
+     *
+     */
+    public function onBeforePageInit()
+    {
+        if ($this->isCMS()) {
+            return;
+        }
+
+        $this->loadGoogleScripts();
+    }
+
+    /**
      * Update the settings tab
      *
      * @param \FieldList
@@ -324,4 +337,44 @@ class MetaConfigPageExtension extends DataExtension
             return $this->owner->PrevLink();
         }
         return false;
-    }}
+    }
+
+    /**
+     * Insert Google Tag Manager or Google Analytics script if their IDs are filled
+     *
+     * @note    Tag Manager has priority over Analytics
+     */
+    protected function loadGoogleScripts()
+    {
+        if ($this->getSiteConfig()->GoogleTagManagerID) {
+            Requirements::insertHeadTags("
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','" . $this->getSiteConfig()->GoogleTagManagerID . "');</script>
+            ");
+        } elseif ($this->getSiteConfig()->AnalyticsID) {
+            Requirements::insertHeadTags("
+                <script>
+                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                    ga('create', '" . $this->getSiteConfig()->AnalyticsID . "', 'auto');
+                    ga('require', 'displayfeatures');
+                    ga('send', 'pageview');
+                </script>
+            ");
+        }
+    }
+
+    /**
+     * Returns true if the uploader is being used in CMS context
+     *
+     * @return boolean
+     */
+    protected function isCMS()
+    {
+        return Controller::curr() instanceof LeftAndMain;
+    }
+}
